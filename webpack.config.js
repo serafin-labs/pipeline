da75f5fe54541da32ad6a7c35e5604920ad06060
Nicolas Degardin <degardin.n@gmail.com>
2018-05-11 17:00:33 -0400

    Webpack build simplified by using awesome typescript loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,51 +4,51 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
 const WebpackShellPlugin = require('webpack-shell-plugin');
 
-// detect production/development mode
-const options = {
-    isProduction: process.env.NODE_ENV === "production",
-    isTest: process.env.BUILD_TYPE === "test"
-};
+// Webpack dev server doesn't support multiconfiguration. We need to detect it to use only the webapp configuration.
+const isWebpackDevServer = process.env.MODE === "wds"
 
-console.log(`Building for production:${options.isProduction} test:${options.isTest}`);
+module.exports = function (env, argv) {
+    const isProduction = !(argv.mode === "development")
+    console.log(`Building mode:${argv.mode || "production"}`);
 
-module.exports = {
-    entry: (options.isTest) ? { "test": "./src/test/tests.ts" } : { "index": "./src/index.ts" },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx']
-    },
-    target: "node",
-    externals: [nodeExternals()],
-    devtool: 'nosources-source-map',
-    watchOptions: {
-        poll: 1000,
-        ignored: /node_modules|lib/
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: {
-                    loader: 'ts-loader',
-                    options: {
-                        configFile: (options.isTest) ? 'tsconfig.test.json' : 'tsconfig.json'
-                    }
+    var configurations = [{
+        entry: {
+            index: "./src/index.ts",
+            test: "./src/test/tests.ts",
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.jsx']
+        },
+        target: "node",
+        externals: [nodeExternals()],
+        devtool: 'nosources-source-map',
+        watchOptions: {
+            poll: 1000,
+            ignored: /node_modules|lib/
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: 'awesome-typescript-loader',
+                    exclude: /node_modules|lib/
                 },
-                exclude: /node_modules/,
-            }
+            ]
+        },
+        optimization: {
+            minimize: false
+        },
+        output: {
+            filename: '[name].js',
+            libraryTarget: 'commonjs',
+            devtoolModuleFilenameTemplate: info => info.resourcePath.startsWith('./src') ? `.${info.resourcePath}` : info.absoluteResourcePath,
+            path: path.resolve(__dirname, 'lib')
+        },
+        plugins: [
+            ...(!isProduction ? [new WebpackShellPlugin({ onBuildExit: ['npm test'] })] : []),
+            ...(isProduction ? [new CleanWebpackPlugin(["lib"])] : []),
         ]
-    },
-    optimization: {
-        minimize: false
-    },
-    output: {
-        filename: '[name].js',
-        libraryTarget: 'commonjs',
-        devtoolModuleFilenameTemplate: info => info.resourcePath.startsWith('./src') ? `.${info.resourcePath}` : info.absoluteResourcePath,
-        path: path.resolve(__dirname, 'lib')
-    },
-    plugins: [
-        ...(options.isTest && !options.isProduction ? [new WebpackShellPlugin({ onBuildExit: ['npm test'] })] : []),
-        ...(!options.isTest && options.isProduction ? [new CleanWebpackPlugin(["lib"])] : []),
-    ]
-};
+    }];
+
+    return isWebpackDevServer ? configurations[0] : configurations
+}
